b6525afb46f2b114508d6c399a5ae113c46f77fc
thomas.tesche <thomas.tesche@idealo.de>
2023-05-26 11:48:10 +0200

    Copy SecurityConfig from 'getting started'

---
+++ springapplication/src/main/java/de/thtesche/springapplication/SpringapplicationApplication.java
@@ -0,0 +1,13 @@
+package de.thtesche.springapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapplicationApplication.class, args);
+	}
+
+}
---
+++ springapplication/src/test/java/de/thtesche/springapplication/SpringapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thtesche.springapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
